efebd75e6d0e1ad77ee4c3fbca406a28ed732780
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-03-27 15:22:44 -0300

    remove UI Layer and modify Presentation layer

--- Presentation/Views/CardsView.swift
+++ Presentation/Views/CardsView.swift
@@ -1,8 +1,47 @@
-//
-//  CardsView.swift
-//  Presentation
-//
-//  Created by Leonardo Almeida on 23/03/23.
-//
-
 import Foundation
+
+public protocol CardsView {
+    func updateCardsView(viewModel: CardsViewViewModel)
+}
+
+public struct CardsViewViewModel {
+    public let cards: [CardModel]
+    
+    public init(cards: [CardModel]) {
+        self.cards = cards
+    }
+}
+
+public struct CardModel {
+    public let isVirtual: Bool
+    public let balance: String
+    public let cardFlag: String
+    public let cardBrandImageUrl: String
+    public let cardTag: Int
+    public let cardNumber: String
+    public let cardExpirationDate: String
+    public let cardFunction: String
+    public let cvc: String
+    
+    public init(
+        isVirtual: Bool,
+        balance: String,
+        cardFlag: String,
+        cardTag: Int,
+        cardBrandImageUrl: String,
+        cardNumber: String,
+        cardExpirationDate: String,
+        cardFunction: String,
+        cvc: String
+    ) {
+        self.isVirtual = isVirtual
+        self.balance = balance
+        self.cardFlag = cardFlag
+        self.cardTag = cardTag
+        self.cardBrandImageUrl = cardBrandImageUrl
+        self.cardNumber = cardNumber
+        self.cardExpirationDate = cardExpirationDate
+        self.cardFunction = cardFunction
+        self.cvc = cvc
+    }
+}
--- PresentationTests/Scenes/Home/HomeRouterTests.swift
+++ PresentationTests/Scenes/Home/HomeRouterTests.swift
@@ -1,8 +1,25 @@
-//
-//  HomeRouterTests.swift
-//  PresentationTests
-//
-//  Created by Leonardo Almeida on 21/03/23.
-//
+import XCTest
+@testable import Presentation
 
-import Foundation
+class HomeRouterTests: XCTestCase {
+    func test_router_should_be_initialized_with_correct_controller() {
+        let homeViewController = HomeController()
+        let sut = makeSut(viewController: homeViewController)
+        XCTAssertEqual(sut.viewController, homeViewController)
+    }
+    
+    func test_router_should_implements_presenterToRouterHomeProtocol() {
+        let sut = makeSut()
+        XCTAssertTrue(sut is PresenterToRouterHomeProtocol)
+    }
+    
+    
+}
+
+extension HomeRouterTests {
+    func makeSut(viewController: UIViewController = HomeController(), file: StaticString = #filePath, line: UInt = #line) -> HomeRouter {
+        let sut = HomeRouter(viewController: viewController)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
+    }
+}
